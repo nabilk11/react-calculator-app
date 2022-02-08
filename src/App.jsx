d5f214e216123af2ffb7e39a009fb37835c29192
nabilk11 <khan.nabiln11@gmail.com>
2022-02-08 01:02:17 -0500

    added initial styling and ui for calculator

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">12354*</div>
+          <div className="current-operand">12565</div>
+        
+      </div>
+      <button className="span-two">AC</button>
+      <button className="">DEL</button>
+      <button className="">/</button>
+      <button className="">1</button>
+      <button className="">2</button>
+      <button className="">3</button>
+      <button className="">*</button>
+      <button className="">4</button>
+      <button className="">5</button>
+      <button className="">6</button>
+      <button className="">+</button>
+      <button className="">7</button>
+      <button className="">8</button>
+      <button className="">9</button>
+      <button className="">-</button>
+      <button className="">.</button>
+      <button className="">0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+}
+
+export default App;
